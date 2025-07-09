46b9d396300674af6d47a1bf733701ee1d95ef28
felipecazotti <felipecazotti@gmail.com>
2025-07-08 22:19:29 -0300

    feat: processador request criacao para cobranca unica

---
+++ Cobranca.Gestao.Domain/ProcessadoresMensagens/ProcessadorCobrancaUnica.cs
@@ -0,0 +1,36 @@
+﻿using Cobranca.Gestao.Domain.ApiModels.Requests;
+using Cobranca.Lib.Dominio.Models;
+using Cobranca.Lib.Dominio.Processadores;
+
+namespace Cobranca.Gestao.Domain.ProcessadoresMensagens;
+
+public static class ProcessadorCobrancaUnica
+{
+    public static CobrancaUnica RequestParaDominio(CriacaoCobrancaRequest request)
+    {
+        if (request == null)
+        {
+            throw new ArgumentNullException(nameof(request), "O objeto de requisição não pode ser nulo.");
+        }
+
+        return new CobrancaUnica
+        {
+            NomeCobranca = request.NomeCobranca,
+            DescricaoCobranca = request.DescricaoCobranca,
+            ValorCobranca = request.ValorCobranca,
+            NomeDevedor = request.NomeDevedor,
+            EmailDevedor = request.EmailDevedor,
+            NomeRecebedor = request.NomeRecebedor,
+            ChavePix = request.ChavePix,
+            QrCode = GeradorPayloadQrCode.GerarPayloadPixEstatico(
+                request.ChavePix,
+                request.NomeDonoChave,
+                request.CidadeDonoChave,
+                request.ValorCobranca),
+            DataHoraRegistroCobranca = DateTime.Now, 
+            DataCobranca = request.DataCobranca ?? throw new ArgumentException(
+                "Data de cobrança é obrigatória.",
+                nameof(request.DataCobranca)),
+        };
+    }
+}
